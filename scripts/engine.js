fdc5afb56e9f5c307150734a9c521f38e48ab50f
Évelim Dornelles <dorrrnelles@gmail.com>
2024-10-03 11:54:06 -0300

    menu lateral

---
+++ scripts/engine.js
@@ -0,0 +1,15 @@
+// Função para alternar o menu
+function toggleMenu() {
+    const menu = document.querySelector('.menu');
+    const menuIcon = document.querySelector('.menu-icon');
+
+    // Alterna a classe 'menu-active' no menu
+    menu.classList.toggle('menu-active');
+
+    // Verifica se o menu está ativo e alterna os ícones
+    if (menu.classList.contains('menu-active')) {
+        menuIcon.innerHTML = '&#10005;'; // Ícone de fechar (X)
+    } else {
+        menuIcon.innerHTML = '&#9776;'; // Ícone de hambúrguer
+    }
+}
